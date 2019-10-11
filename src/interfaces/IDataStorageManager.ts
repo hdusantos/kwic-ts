050d88543d74170cb5bbd5a177a84d4e0dd0894f
Hudson dos Santos <hudson.alves@live.com>
2019-10-11 15:29:35 -0300

    Added Interfaces

---
+++ src/interfaces/IDataStorageManager.ts
@@ -0,0 +1,22 @@
+interface IDataStorageManager {
+
+  /**
+   * Initialize the DataStorageManager
+   */
+  init(): void;
+
+  /**
+   * Get a specific item on the data storage
+   * @param {number} idx index for a data storage item
+   * @return {string} a specific item on the data storage (String)
+   */
+  line(idx: number): string;
+
+  /**
+   * The total number of lines of a data storage
+   * @return {number} total number of lines (Int)
+   */
+  length(): number;
+}
+
+export default DataStorageManager;
---
+++ src/interfaces/IIndexManager.ts
@@ -0,0 +1,31 @@
+interface IIndexManager {
+  /**
+   * Returns true if index manager is empty
+   * @return {boolean} true if index manager is empty
+   */
+  isEmpty(): boolean;
+
+  /**
+   * @param {string} word word to be indexed
+   * @param {string} line line where the word appears
+   * @param {number} pos word position on the line
+   */
+  map(word: string, line: string, pos: number): void;
+
+  /**
+   * Retrieves occurrence of a word
+   * @param {string} word word to be searched
+   * @return {string} Array containing the lines on which a word
+   *                  appears and the position on the line
+   */
+
+  occurrencesOfWord(word: string): [];
+
+  /**
+   * Sorted All Words
+   * @return {string[]} Array containing all sorted words
+   */
+  sortedWords(): string[];
+}
+
+export default IIndexManager;
