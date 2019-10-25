73bc00bb7f3973c1fb79746b1464087ce8c6fce6
Hudson dos Santos <hudson.alves@live.com>
2019-10-25 10:43:56 -0300

    Update version v1.1.0

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,8 @@
+import IDataStorageManager from "./interfaces/IDataStorageManager";
+import IIndexManager from "./interfaces/IIndexManager";
+import IOutputManager from "./interfaces/IOutputManager";
+import IStopWordManager from "./interfaces/IStopWordManager";
+import IWordShift from "./interfaces/IWordShift";
 import Kwic from "./kwicManager/KwicManager";
+export { IDataStorageManager, IIndexManager, IOutputManager, IStopWordManager, IWordShift, };
 export default Kwic;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+/* Interfaces */
+import IDataStorageManager from "./interfaces/IDataStorageManager";
+import IIndexManager from "./interfaces/IIndexManager";
+import IOutputManager from "./interfaces/IOutputManager";
+import IStopWordManager from "./interfaces/IStopWordManager";
+import IWordShift from "./interfaces/IWordShift";
+/* App */
 import Kwic from "./kwicManager/KwicManager";
+
+export {
+  IDataStorageManager,
+  IIndexManager,
+  IOutputManager,
+  IStopWordManager,
+  IWordShift,
+};
 
 export default Kwic;
